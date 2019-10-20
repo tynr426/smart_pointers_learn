e8ddd9ccb0513fe49f2cf3892a45faf183aca84f
apple <tynr426@126.com>
2019-10-20 16:43:52 +0800

    解引用强制多态

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,39 @@ mod my_box;
 mod my_rc;
 use my_box::MyBox;
 mod ref_cell;
-use ref_cell::{LimitTracker,MockMessenger};
+use ref_cell::{LimitTracker, MockMessenger};
 use std::cell::RefCell;
 fn main() {
     let x = 5;
     let y = MyBox::new(x);
 
     assert_eq!(5, x);
-    assert_eq!(5, *y);
-    let xx = MyBox{value:"Rust"};
+    assert_eq!(5, *y); //*y=*(y.deref())
+    let xx = MyBox { value: "Rust" };
     assert_eq!("Rust", *xx);
     let m = MyBox::new(String::from("Rust"));
-    assert_eq!(*xx,*m);
+    //解引用强制多态所以能成功，标准库中提供了 String 上的 Deref 实现，有两次解引用
+    //一个是MyBox,一个是String
+    hello(&m);//=hello(&(*m)[..]);
+    assert_eq!(*xx, *m);
     my_rc::test();
     it_sends_an_over_75_percent_warning_message();
 }
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
 
-    fn it_sends_an_over_75_percent_warning_message() {
-        let mock_messenger = MockMessenger::new();
-        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+fn it_sends_an_over_75_percent_warning_message() {
+    let mock_messenger = MockMessenger::new();
+    let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
 
-        limit_tracker.set_value(80);
-        let f:std::cell::Ref<'_, std::vec::Vec<std::string::String>>=mock_messenger.sent_messages.borrow();
-        println!("{:?}",f);
-        println!("{:?},{:?}",mock_messenger.sent_messages.borrow(),(mock_messenger.sent_messages));
-    }
-
+    limit_tracker.set_value(80);
+    let f: std::cell::Ref<'_, std::vec::Vec<std::string::String>> =
+        mock_messenger.sent_messages.borrow();
+    println!("{:?}", f);
+    println!(
+        "{:?},{:?}",
+        mock_messenger.sent_messages.borrow(),
+        (mock_messenger.sent_messages)
+    );
+}
--- src/my_box.rs
+++ src/my_box.rs
@@ -1,3 +1,6 @@
+/*
+box 只提供了间接存储和堆分配；他们并没有任何其他特殊的功能
+*/
 use std::ops::Deref;
 pub struct MyBox<T>{
     pub value:T
@@ -9,7 +12,7 @@ impl<T> MyBox<T> {
     }
 }
 impl<T> Deref for MyBox<T> {
-    type Target = T;
+    type Target = T;//语法定义了用于此 trait 的关联类型。关联类型是一个稍有不同的定义泛型参数的方式
 
     fn deref(&self) -> &T {
         &self.value
